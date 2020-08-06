8ff1510c74318be6c932cceb2f4e782e067bc392
= <=>
2020-08-06 19:57:09 -0400

    Task 1 almost done

---
+++ generator.py
@@ -0,0 +1,47 @@
+"""
+Program: generator.py
+Generates and displays sentences using simple grammar
+and vocabulary. Words are chosen at random.
+"""
+
+import random
+
+
+def getWords(filename):
+    words_list = []
+    with open(filename) as file:
+        for line in file:
+            words_list.append(line.strip())
+    
+    return tuple(words_list)
+
+
+articles = getWords("articles.txt")
+nouns = getWords("nouns.txt")
+verbs = getWords("verbs.txt")
+prepositions = getWords("prepositions.txt")    
+    
+def sentence():
+    """Builds and returns a sentence."""
+    return nounPhrase() + " " + verbPhrase()
+
+def nounPhrase():
+    """Builds and returns a noun phrase."""
+    return random.choice(articles) + " " + random.choice(nouns)
+
+def verbPhrase():
+    """Builds and returns a verb phrase."""
+    return random.choice(verbs) + " " + nounPhrase() + " " + prepositionalPhrase()
+
+def prepositionalPhrase():
+    """Builds and returns a prepositional phrase."""
+    return random.choice(prepositions) + " " + nounPhrase()
+
+def main():
+    """Allows the user to input the number of sentences
+    to generate."""
+    number = int(input("Enter the number of sentences: "))
+    for count in range(number):
+        print(sentence())
+        
+main()
